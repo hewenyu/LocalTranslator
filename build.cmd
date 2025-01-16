71f8fe9be294de7e56ccc8acd51ea4fd7e79912a
hewenyu <yuebanlaosiji@outlook.com>
2025-01-16 23:21:15 +0800

    demo

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-cmd /c "rmdir /s /q build && cmake -B build -S . -DCMAKE_INSTALL_PREFIX="./vcpkg_installed/x64-windows;./onnxruntime/onnxruntime-win-x64"
+cmd /c "rmdir /s /q build && cmake -B build -S . -DCMAKE_INSTALL_PREFIX="./onnxruntime/onnxruntime-win-x64"
 
 echo Building project...
 cmake --build build
